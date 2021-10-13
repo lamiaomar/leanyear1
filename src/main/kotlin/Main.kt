d0131c08f6ab16ec496014892095819080f6e296
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2021-10-13 08:56:45 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    var yearnum = 1300
+
+    if(yearnum>1582){
+        if(yearnum%4000 == 0){println("it is lean") }
+        else if (yearnum%100 == 0){ println("not lean")}
+        else if (yearnum%100==0){println("it is lean ")}}
+    if (yearnum<1582){
+        println("it is not lean year")}
+    }
+
+
